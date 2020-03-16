51163447b4c610cf90206da201b2db74708caf88
Mhburg <Morgan_lee5@hotmail.com>
2020-03-16 00:32:40 -0400

    Update Jobs

---
+++ src/Common/AwesomeInventoryBase/Resources/UIText.cs
@@ -0,0 +1,104 @@
+﻿// <copyright file="UIText.cs" company="Zizhen Li">
+// Copyright (c) Zizhen Li. All rights reserved.
+// Licensed under the GPL-3.0-only license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomeInventory.Base.Resources
+{
+    /// <summary>
+    /// Text displayed on UI. Call Translate() on members whenever they are referenced, e.g., UIText.Equip.Translate(); .
+    /// </summary>
+    public static class UIText
+    {
+#pragma warning disable SA1600 // Elements should be documented
+        public const string ArmorBlunt = "ArmorBlunt";
+
+        public const string ArmorHeat = "ArmorHeat";
+
+        public const string ArmorSharp = "ArmorSharp";
+
+        public const string Delete = "Delete";
+
+        public const string Equip = "Equip";
+
+        public const string EquipWarningBrawler = "EquipWarningBrawler";
+
+        public const string Inventory = "Inventory";
+
+        public const string OK = "OK";
+
+        public const string AIEquip = "AwesomeInventory_Equip";
+
+        public const string AIForceWear = "AwesomeInventory_ForceWear";
+
+        public const string AIWear = "AwesomeInventory_Wear";
+
+        public const string AsceticTab = "AwesomeInventory_Ascetic";
+
+        public const string ChooseMaterialAndQuality = "AwesomeInventory_ChooseMaterialAndQuality";
+
+        public const string ComfyTemperatureRange = "ComfyTemperatureRange";
+
+        public const string CopyLoadout = "AwesomeInventory_CopyLoadout";
+
+        public const string CountFieldTip = "AwesomeInventory_CountFieldTip";
+
+        public const string Customize = "AwesomeInventory_Customize";
+
+        public const string DeleteLoadout = "AwesomeInventory_DeleteLoadout";
+
+        public const string DragToReorder = "AwesomeInventory_DragToReorder";
+
+        public const string GreedyTab = "AwesomeInventory_GreedyTab";
+
+        public const string JealousTab = "AwesomeInventory_JealousTab";
+
+        public const string MassCarried = "AwesomeInventory_MassCarried";
+
+        public const string MouseOverNumbersForDetails = "AwesomeInventory_MouseOverNumbersForDetails";
+
+        public const string NewLoadout = "AwesomeInventory_NewLoadout";
+
+        public const string NoLoadout = "AwesomeInventory_NoLoadout";
+
+        public const string NoLoadoutSelected = "AwesomeInventory_NoLoadoutSelected";
+
+        public const string NoMaterial = "AwesomeInventory_NoMaterial";
+
+        public const string OpenLoadout = "AwesomeInventory_OpenLoadout";
+
+        public const string PutAway = "AwesomeInventory_PutAway";
+
+        public const string SelectLoadout = "AwesomeInventory_SelectLoadout";
+
+        public const string SourceAllTip = "AwesomeInventory_SourceAllTip";
+
+        public const string SourceFilterTip = "AwesomeInventory_SourceFilterTip";
+
+        public const string SourceGenericTip = "AwesomeInventory_SourceGenericTip";
+
+        public const string SourceMeleeTip = "AwesomeInventory_SourceMeleeTip";
+
+        public const string SourceMinifiedTip = "AwesomeInventory_SourceMinifiedTip";
+
+        public const string SourceRangedTip = "AwesomeInventory_SourceRangedTip";
+
+        public const string Weight = "AwesomeInventory_Weight";
+
+        public const string OutfitAnything = "OutfitAnything";
+
+        public const string SourceApparelTip = "SourceApparelTip";
+
+        public const string TenCharsString = "AAAAAAAAAA";
+
+        public const string Traits = "Traits";
+
+#pragma warning restore SA1600 // Elements should be documented
+    }
+}
