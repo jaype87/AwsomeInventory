c93a4c7520c0f08acf4ebe269118c7937fba9d62
Mhburg <Morgan_lee5@hotmail.com>
2020-03-22 14:38:51 -0400

    Fix one pair of double quotes enclose both source path and destination path

--- SolutionItems/Deploy.bat
+++ SolutionItems/Deploy.bat
@@ -1,23 +1,23 @@
-@echo off
+REM @echo off
 REM %1 should be the $(ProjectDir) macro in msbuild.
 REM %2 should be the $(OutDir) macro in msbuild.
 
 SET _defs=Defs
 IF EXIST "%~dp1%_defs%" (
-    xcopy /i /e /d /y "%~dp1%_defs% %~dp2..\%_defs%"
+    xcopy /i /e /d /y "%~dp1%_defs%" "%~dp2..\%_defs%"
 )
 
 SET _languages=Languages
 IF EXIST "%~dp1%_languages%" (
-    xcopy /i /e /d /y "%~dp1%_languages% %~dp2..\%_languages%"
+    xcopy /i /e /d /y "%~dp1%_languages%" "%~dp2..\%_languages%"
 )
 
 SET _patches=Patches
 IF EXIST "%~dp1%_patches%" (
-    xcopy /i /e /d /y "%~dp1%_patches% %~dp2..\%_patches%"
+    xcopy /i /e /d /y "%~dp1%_patches%" "%~dp2..\%_patches%"
 )
 
 SET _textures=Textures
 IF EXIST "%~dp1%_textures%" (
-    xcopy /i /e /d /y "%~dp1%_textures% %~dp2..\%_textures%"
+    xcopy /i /e /d /y "%~dp1%_textures%" "%~dp2..\%_textures%"
 )
