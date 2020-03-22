87d4397d33db548ea7903d26bd0b5d5de1743f7a
Jonathan Troyer <jonathantroyer@outlook.com>
2020-03-22 12:57:55 -0400

    Add colors from other popular RimWorld coloring mods

--- src/V1.1/Common/UI/AwesomeInventoryTex.cs
+++ src/V1.1/Common/UI/AwesomeInventoryTex.cs
@@ -21,10 +21,32 @@ namespace AwesomeInventory.UI
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning disable SA1600 // Elements should be documented
 
+        /************
+         *  Colors  *
+         ************/
         public static readonly Color Valvet = GenColor.FromHex("cc1a00");
         public static readonly Color LightGrey = GenColor.FromHex("b0b3af");
         public static readonly Color RWPrimaryColor = GenColor.FromHex("6a512e");
 
+        // For Lighter Colorful Traits
+        public static readonly Color LCT_MediumPurple = GenColor.FromHex("9370DB");
+        public static readonly Color LCT_Olivine = GenColor.FromHex("9AB973");
+        public static readonly Color LCT_CanaryYellow = GenColor.FromHex("FFFF99");
+        public static readonly Color LCT_AtomicTangerine = GenColor.FromHex("FF9966");
+        public static readonly Color LCT_PantoneRed = GenColor.FromHex("ED2939");
+
+        // For Color Coded Mood Bar
+        private static float colorAlpha = 0.44f;
+        public static readonly Color CCMB_HappyColor = new Color(0.1f, 0.75f, 0.2f, colorAlpha);
+        public static readonly Color CCMB_Cyan = new Color(Color.cyan.r, Color.cyan.g, Color.cyan.b, colorAlpha);
+        public static readonly Color CCMB_NeutralColor = new Color(0.87f, 0.96f, 0.79f, colorAlpha);
+        public static readonly Color CCMB_Yellow = new Color(Color.yellow.r, Color.yellow.g, Color.yellow.b, colorAlpha);
+        public static readonly Color CCMB_Orange = new Color(1f, 0.5f, 0.31f, colorAlpha);
+        public static readonly Color CCMB_Red = new Color(Color.red.r, Color.red.g, Color.red.b, colorAlpha);
+
+        /************
+         * Textures *
+         ************/
         public static readonly Texture OrangeTex = SolidColorMaterials.NewSolidColorTexture(ColorLibrary.Orange);
         public static readonly Texture CyanTex = SolidColorMaterials.NewSolidColorTexture(ColorLibrary.Cyan);
         public static readonly Texture BrightPurpleTex = SolidColorMaterials.NewSolidColorTexture(ColorLibrary.BrightPurple);
@@ -47,6 +69,24 @@ namespace AwesomeInventory.UI
         public static readonly Texture Normal = BaseContent.WhiteTex;
         public static readonly Texture Poor = LightGreyTex;
         public static readonly Texture Awful = ValvetTex;
+
+        // For Lighter Colorful Traits
+        public static readonly Texture LCT_Lengendary = SolidColorMaterials.NewSolidColorTexture(LCT_MediumPurple);
+        public static readonly Texture LCT_Masterwork = CyanTex; // TODO: possibly tweak this?
+        public static readonly Texture LCT_Excellent = SolidColorMaterials.NewSolidColorTexture(LCT_Olivine);
+        public static readonly Texture LCT_Good = BaseContent.WhiteTex;
+        public static readonly Texture LCT_Normal = SolidColorMaterials.NewSolidColorTexture(LCT_CanaryYellow);
+        public static readonly Texture LCT_Poor = SolidColorMaterials.NewSolidColorTexture(LCT_AtomicTangerine);
+        public static readonly Texture LCT_Awful = SolidColorMaterials.NewSolidColorTexture(LCT_PantoneRed);
+
+        // For Color Coded Mood Bar
+        public static readonly Texture CCMB_Lengendary = BrightPurpleTex; // TODO: possibly tweak this?
+        public static readonly Texture CCMB_Masterwork = SolidColorMaterials.NewSolidColorTexture(CCMB_HappyColor);
+        public static readonly Texture CCMB_Excellent = SolidColorMaterials.NewSolidColorTexture(CCMB_Cyan);
+        public static readonly Texture CCMB_Good = SolidColorMaterials.NewSolidColorTexture(CCMB_NeutralColor);
+        public static readonly Texture CCMB_Normal = SolidColorMaterials.NewSolidColorTexture(CCMB_Yellow);
+        public static readonly Texture CCMB_Poor = SolidColorMaterials.NewSolidColorTexture(CCMB_Orange);
+        public static readonly Texture CCMB_Awful = SolidColorMaterials.NewSolidColorTexture(CCMB_Red);
 
 #pragma warning restore SA1600 // Elements should be documented
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
