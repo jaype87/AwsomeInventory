e231ca161396092bd505e703328ceeccabce4546
Mhburg <Morgan_lee5@hotmail.com>
2020-04-07 01:26:59 -0400

    Add cache for ThingSample to avoid contant changes in Thing.Label

--- src/V1.1/Common/Loadout/SingleThingSelector.cs
+++ src/V1.1/Common/Loadout/SingleThingSelector.cs
@@ -21,6 +21,7 @@ namespace AwesomeInventory.Loadout
     public class SingleThingSelector : ThingSelector, IEquatable<SingleThingSelector>
     {
         private ThingDef _allowedStuff;
+        private Thing _thingSample;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SingleThingSelector"/> class.
@@ -103,10 +104,13 @@ namespace AwesomeInventory.Loadout
         /// </summary>
         public Thing ThingSample
         {
-            get =>
-                this.AllowedThing.HasComp(typeof(CompQuality))
-                ? new ThingStuffPairWithQuality(this.AllowedThing, this.AllowedStuff, _thingFilter.AllowedQualityLevels.min).MakeThingWithoutID()
-                : new ThingStuffPair(this.AllowedThing, this.AllowedStuff).MakeThingWithoutID();
+            get
+            {
+                if (_dirty)
+                    this.UpdateReadout();
+
+                return _thingSample;
+            }
         }
 
         /// <summary>
@@ -160,6 +164,7 @@ namespace AwesomeInventory.Loadout
         public void SetStuff(ThingDef stuffDef)
         {
             _allowedStuff = stuffDef;
+            _dirty = true;
         }
 
         /// <summary>
@@ -211,6 +216,18 @@ namespace AwesomeInventory.Loadout
         {
             base.ExposeData();
             Scribe_Defs.Look(ref _allowedStuff, nameof(_allowedStuff));
+        }
+
+        /// <summary>
+        /// Update readout for this selector when quality level, hit point percentage or stuff source is changed.
+        /// </summary>
+        protected virtual void UpdateReadout()
+        {
+            _thingSample = this.AllowedThing.HasComp(typeof(CompQuality))
+                ? new ThingStuffPairWithQuality(this.AllowedThing, this.AllowedStuff, _thingFilter.AllowedQualityLevels.min).MakeThingWithoutID()
+                : new ThingStuffPair(this.AllowedThing, this.AllowedStuff).MakeThingWithoutID();
+
+            _dirty = false;
         }
 
         /// <summary>
